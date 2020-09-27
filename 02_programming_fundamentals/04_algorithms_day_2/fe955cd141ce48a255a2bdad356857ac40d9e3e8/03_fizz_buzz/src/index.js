690250d80a3c840768ed273ab2200fc7369c38e6
damien <damien.skrzypczak@gmail.com>
2020-09-27 17:01:56 +0200

    exofizzbuzz

--- 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/03_fizz_buzz/src/index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/03_fizz_buzz/src/index.js
@@ -1,6 +1,18 @@
-function fizzBuzz() {
-  // Code the function here.
+function fizzBuzz(list) {
+  const NewArray = [];
+
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] % 3 === 0 && list[i] % 5 === 0) {
+      NewArray.push("FizzBuzz");
+    } else if (list[i] % 3 === 0) {
+      NewArray.push("Fizz");
+    } else if (list[i] % 5 === 0) {
+      NewArray.push("Buzz");
+    } else {
+      NewArray.push(list[i]);
+    }
+  }
+  return NewArray;
 }
 
-// Leave the line below untouched for tests to work properly.
 module.exports = fizzBuzz;
